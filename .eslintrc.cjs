b7f1b7b90fb9e0fcf34fc8d4bd04954796ca3e4b
Kay Kim <kimjh@bawi.org>
2022-01-11 18:59:31 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,65 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['import', 'unused-imports', 'svelte3', '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'import/newline-after-import': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: '$app/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '$utils/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '$lib/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '$components/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'unknown',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+      },
+    ],
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
